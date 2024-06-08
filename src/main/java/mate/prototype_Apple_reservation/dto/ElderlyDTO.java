b39bf57b2e8229cb0201343cf0f891315700cc8e
jmsparrowk <jmsparrowk@naver.com>
2024-06-08 22:23:36 +0900

    6/8

--- src/main/java/mate/prototype_Apple_reservation/dto/ElderlyDTO.java
+++ src/main/java/mate/prototype_Apple_reservation/dto/ElderlyDTO.java
@@ -1,7 +1,11 @@
 package mate.prototype_Apple_reservation.dto;
 
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.antlr.v4.runtime.misc.NotNull;
 
 @Getter
 @Setter
