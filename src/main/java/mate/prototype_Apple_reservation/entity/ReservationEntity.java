fcfab3491363b12ae91302122bc1534e508b1353
jmsparrowk <jmsparrowk@naver.com>
2024-06-16 12:38:18 +0900

    6/16

--- src/main/java/mate/prototype_Apple_reservation/entity/ReservationEntity.java
+++ src/main/java/mate/prototype_Apple_reservation/entity/ReservationEntity.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import mate.prototype_Apple_reservation.enums.MealType;
+import mate.prototype_Apple_reservation.enums.ReservationType;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
