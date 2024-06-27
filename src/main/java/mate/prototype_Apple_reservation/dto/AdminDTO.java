a80618c9983b8031d0ab645fd6d6cbb8bbd9e3d1
jmsparrowk <jmsparrowk@naver.com>
2024-06-27 23:12:49 +0900

    6/27

--- src/main/java/mate/prototype_Apple_reservation/dto/AdminDTO.java
+++ src/main/java/mate/prototype_Apple_reservation/dto/AdminDTO.java
@@ -8,6 +8,7 @@ import lombok.*;
 @NoArgsConstructor //기본 생성자
 @AllArgsConstructor //모든 필드를 매개변수로 하는 생성자
 public class AdminDTO {
-    public String name;
-    public int password;
+    private Long adminId;
+    private String name;
+    private int password;
 }
