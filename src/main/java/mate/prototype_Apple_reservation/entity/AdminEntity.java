7273f59b6d02350f1b6e1389913c24d8da4879ad
jmsparrowk <jmsparrowk@naver.com>
2024-07-01 21:46:20 +0900

    7/1

--- src/main/java/mate/prototype_Apple_reservation/entity/AdminEntity.java
+++ src/main/java/mate/prototype_Apple_reservation/entity/AdminEntity.java
@@ -1,7 +1,16 @@
 package mate.prototype_Apple_reservation.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class AdminEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long elderlyId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int floor;
 }
