52f8e24ef28ade896302525389af96b2f5925576
jmsparrowk <jmsparrowk@naver.com>
2024-07-04 23:46:46 +0900

    7/4

--- src/main/java/mate/prototype_Apple_reservation/controller/api/API_ReservationController.java
+++ src/main/java/mate/prototype_Apple_reservation/controller/api/API_ReservationController.java
@@ -1,9 +1,20 @@
 package mate.prototype_Apple_reservation.controller.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
 public class API_ReservationController {
+    // 입소자 명부 모두 조회
+    // http://localhost:8080/elderly
+    @GetMapping
+    public List<ElderlyDTO> getAllElderly() {
+        return elderlyService.findAll();
+    }
+
 }
+
