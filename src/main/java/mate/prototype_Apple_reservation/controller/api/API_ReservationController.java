74735385a96c298faa55d9361d6ddc9eb1b0c2f4
jmsparrowk <jmsparrowk@naver.com>
2024-07-06 23:16:32 +0900

    7/6

--- src/main/java/mate/prototype_Apple_reservation/controller/api/API_ReservationController.java
+++ src/main/java/mate/prototype_Apple_reservation/controller/api/API_ReservationController.java
@@ -24,5 +24,29 @@ public class API_ReservationController {
         return ResponseEntity.ok().build();
     }
 
+    // 입소자 명부 수정
+    // http://localhost:8080/elderly/{id}
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateElderly(@PathVariable("id") Long id, @Valid @RequestBody ElderlyDTO elderlyDetails) {
+        elderlyService.updateElderly(id, elderlyDetails);
+        return ResponseEntity.ok().build();
+    }
+
+    // 입소자 명부 삭제
+    // http://localhost:8080/elderly/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteElderly(@PathVariable("id") Long id) {
+        elderlyService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    // 어르신 이름 검증
+    // http://localhost:8080/elderly/findByName?name={name}
+    @GetMapping("/findByName")
+    public ResponseEntity<Long> getElderlyIdByName(@RequestParam("name") String name) {
+        ElderlyDTO elderly = elderlyService.findByName(name);
+        return ResponseEntity.ok(elderly.getElderlyId());
+    }
+
 }
 
